9129c1e9fb4b844132ba9f1557e44e32e32c9a35
Cian Mac Mahon <cmacmahon@HubSpot.com>
2015-05-03 20:56:34 +0100

    Circle in rectangle

--- src/main.c
+++ src/main.c
@@ -1,24 +1,38 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static Layer *myLayer;
 
+static void my_layer_draw(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+  
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  const int16_t half_h = bounds.size.h / 2;
+  graphics_fill_circle(ctx, GPoint(half_h, half_h), half_h);
+  
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  const int16_t half_h2 = bounds.size.h / 3;
+  graphics_fill_circle(ctx, GPoint(half_h, half_h), half_h2);
+}
+  
 static void window_load(Window *window) {
   // get window root layer and bounds
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-    
-  // create a text layer
-  text_layer = text_layer_create((GRect) { .origin = {0,45 }, .size = { bounds.size.w, 80 } });
-  text_layer_set_text(text_layer, "This is another layer!");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-    
-  // add text layer to window root
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+  // create a layer
+  myLayer = layer_create((GRect) { . origin = {0,72}, .size = { bounds.size.w, 50 }});
+  layer_set_update_proc(myLayer, my_layer_draw);
+  
+  // Add layer to window root layer
+  layer_add_child(window_layer, myLayer);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  layer_destroy(myLayer);
 }
 
 static void init(void) {
@@ -30,11 +44,10 @@ static void init(void) {
     .unload = window_unload
   });
   window_stack_push(window, true);
-    
+  
 }
 
 static void deinit(void) {
-  text_layer_destroy(text_layer);
   window_destroy(window);
 }
 
