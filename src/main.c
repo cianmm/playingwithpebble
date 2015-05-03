10790a09ee31e5331ffc7199534d5e6f049c0176
Cian Mac Mahon <cmacmahon@HubSpot.com>
2015-05-03 15:43:48 +0100

    window handlers

--- src/main.c
+++ src/main.c
@@ -3,28 +3,34 @@
 static Window *window;
 static TextLayer *text_layer;
 
-static void init(void) {
-
-  // Create and display a window
-  window = window_create();
-  window_stack_push(window, true);
-    
+static void window_load(Window *window) {
   // get window root layer and bounds
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  
-  Layer *another_layer = layer_create(bounds);
-  
+    
   // create a text layer
   text_layer = text_layer_create((GRect) { .origin = {0,45 }, .size = { bounds.size.w, 80 } });
   text_layer_set_text(text_layer, "This is another layer!");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  
-  layer_add_child(another_layer, text_layer_get_layer(text_layer));
-  
+    
   // add text layer to window root
-  layer_add_child(window_layer, another_layer);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
 
+static void window_unload(Window *window) {
+  text_layer_destroy(text_layer);
+}
+
+static void init(void) {
+
+  // Create and display a window
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload
+  });
+  window_stack_push(window, true);
+    
 }
 
 static void deinit(void) {
